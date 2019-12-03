a6b0456923258ce95bd152c8c8bd1364529407f6
Luis Briones <21979360+lebriones@users.noreply.github.com>
2019-12-02 23:43:20 -0700

    Added lab 6 files

---
+++ disjoint_set_forest.py
@@ -0,0 +1,28 @@
+class DSF:
+    def __init__(self, n):
+        self.forest = [-1] * n
+
+    def is_index_valid(self, index):
+        return 0 <= index < len(self.forest)
+
+    def find(self, a):
+        if not self.is_index_valid(a):
+            return -1
+
+        if self.forest[a] < 0:
+            return a
+
+        return self.find(self.forest[a])
+
+    def find_loop(self, p, q):
+        return self.find(p) == self.find(q)
+
+    def union(self, a, b):
+        ra = self.find(a)
+        rb = self.find(b)
+
+        if ra != rb:  # Problems 3 and 4
+            self.forest[rb] = ra
+
+    def __str__(self):
+        return str(self.forest)
